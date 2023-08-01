b36bb4d1b3d6fbc6bbeeae4b9beac5635653d60c
KiloSum <1412198378@qq.com>
2023-08-02 00:14:30 +0800

    feat: 登录页到详情页的导航特殊处理

--- src/components/NavBarAgain.jsx
+++ src/components/NavBarAgain.jsx
@@ -1,10 +1,24 @@
 import React from "react";
 import { NavBar } from "antd-mobile";
 import PropTypes from "prop-types";
+import { useNavigate, useLocation, useSearchParams } from 'react-router-dom';
+import './NavBarAgain.less';
 const NavBarAgain = function NavBarAgain(props){
   let {title} = props;
-  const handleBack =()=>{};
-  return <NavBar onBack={handleBack}>
+  const navigate = useNavigate(),
+        location = useLocation(),
+        [usp] = useSearchParams();
+  const handleBack =()=>{
+    // 特殊:登录页 & to的值是/deatil/xxx
+    let to = usp.get('to');
+    if (location.pathname === '/login' && /^\/detail\/\d+$/.test(to)) {
+        navigate(to, { replace: true });
+        return;
+    }
+    navigate(-1);
+  };
+  return <NavBar className="navbar-again-box"
+  onBack={handleBack}>
     {title}
   </NavBar>
 };
