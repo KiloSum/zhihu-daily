c2f7287193fa0ce312b8ce41d588836c619a0c31
KiloSum <1412198378@qq.com>
2023-08-01 23:33:45 +0800

    feat: 登录页

--- src/store/reducer/base.js
+++ src/store/reducer/base.js
@@ -1,15 +1,17 @@
-// eslint-disable-next-line
 import * as TYPES from '../action-types';
-import _ from '../../assets/utils'
+import _ from '../../assets/utils';
 
 let initial = {
-  info: null
+    info: null
 };
-export default function baseReducer(state = initial , action){
-  state = _.clone(state);
-  switch(action.type){
-    default:
-  }
-  return state;
-
+export default function baseReducer(state = initial, action) {
+    state = _.clone(state);
+    switch (action.type) {
+        // 更新登录者信息
+        case TYPES.BASE_INFO:
+            state.info = action.info;
+            break;
+        default:
+    }
+    return state;
 };
